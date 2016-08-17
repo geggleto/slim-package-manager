7ae4f6e4e1467ae72b02079019948e7399838d5a
Glenn Eggleton <geggleto@gmail.com>
2016-08-17 18:23:32 -0400

    Update PackageManager.php

--- PackageManager.php
+++ PackageManager.php
@@ -11,8 +11,8 @@ namespace SlimPackage;
 
 class PackageManager
 {
-    protected $app;
-    protected $container;
+    private $app;
+    private $container;
 
     public function __construct(\Slim\App $app)
     {
@@ -25,8 +25,10 @@ class PackageManager
         if (!file_exists($path)) {
             throw new \InvalidArgumentException("Package File Does Not Exist");
         }
-
+        
+        $this->setNamespace("");
         include $path;
+        $this->setNamespace("");
     }
 
     public function registerTemplateDirectory($path) {
@@ -36,5 +38,23 @@ class PackageManager
     public function override($routeName, $callable) {
         $route = $this->app->router->getNamedRoute($routeName);\
         $route->setCallable($callable); //Will be fixed with /slim/slim/#1963
+    }
+    
+    protected function setNamespace($namespace) {
+        $this->namespace = (string)$namespace;
+    }
+    
+    protected function getApp() {
+        if (!empty($this->namespace))
+            return $this->app;
+        else 
+            throw new Exception("Namespace must be set");
+    }
+    
+    protected function getContainer() {
+        if (!empty($this->namespace))
+            return $this->container;
+        else 
+            throw new Exception("Namespace must be set");
     }
 }
